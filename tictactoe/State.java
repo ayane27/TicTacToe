4090806e9aebf425d0a4af05247027532ef587e2
Ayane Sok <ayane.sok@mrjaffesclass.com>
2024-01-25 11:34:43 -0800

    UI

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -10,5 +10,47 @@ public class State
     private String xName = "";
     private String oName = "";
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+    
+    public int getGameState() {
+        return this.gameState;
+    }
+
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+
+    public int getWhoseMove() {
+        return this.whoseMove;
+    }
+    
+    public void setWhoseMove(int whoseMove) {
+        this.whoseMove = whoseMove;
+    }
+    
+    public String getXName() {
+        return this.xName;
+    }
+    
+    public void setXName(String xName) {
+        this.xName = xName;   
+    }
+    
+    public String oName() {
+        return this.oName;
+    }
+    
+    public void setOName(String oName) {
+        this.oName = oName;
+    }
+    public int getBoardCell(int row, int col) {
+	    return this.board[row][col];
+	}
+
+    public void setBoardCell(int row, int col, int value) {
+	    this.board[row][col] = value;
+	}
+    
+    
 }
+    
 
---
+++ tictactoe/UI.java
@@ -0,0 +1,15 @@
+package tictactoe;
+/**
+ * UI class
+ */
+import java.util.Scanner;
+public class UI
+{
+
+    Scanner scanner;
+
+    public UI() {
+        scanner = new Scanner(System.in);         
+    }
+}
+
