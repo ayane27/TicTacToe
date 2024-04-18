480b84fb8047826972a8504c016b6db9a944cae7
ayane27 <143216545+ayane27@users.noreply.github.com>
2024-04-17 19:52:30 -0700

    Update Constants.java

--- tictactoe/Constants.java
+++ tictactoe/Constants.java
@@ -7,7 +7,8 @@ package tictactoe;
 public class Constants
 {
     // Valid board size
-    public static final int BOARD_SIZE = 3;
+    public static final int BOARD_SIZE_ROW = 6;
+    public static final int BOARD_SIZE_COLUMN = 7;
 
     // Valid board values
     public static final int X = -1;    // Indicates an "X"
@@ -29,14 +30,13 @@ public class Constants
     public static final int QUIT_PROGRAM = 11;
     
     // Strings
-    public static final String DIVIDER_STRING = "|---|---|---|";
-    public static final String BOARD_STRING = "| %s | %s | %s |";
+    public static final String DIVIDER_STRING = "|---|---|---|---|---|---|---|";
+    public static final String BOARD_STRING = "| %s | %s | %s | %s | %s | %s |";
     public static final String GET_PLAYER_NAME = "Player %s: What is your name?-->";
-    public static final String TITLE = "Thanks for playing Tic-Tac-Toe!";
-    public static final String GET_ROW_MOVE = "Player %s (%s): Enter the row for your next move-->";
+    public static final String TITLE = "Thanks for playing Connect Four!";
     public static final String GET_COL_MOVE = "Player %s (%s): Enter the column for your next move-->";
-    public static final String INVALID_ROW_OR_COLUMN = "Your row and column must be 1, 2, or 3";
-    public static final String INVALID_MOVE_ERROR = "Row %d column %d is not a valid move. Please try again";
+    public static final String INVALID_ROW_OR_COLUMN = "Your column must be 1, 2, 3, 4, 5, 6, or 7";
+    public static final String INVALID_MOVE_ERROR = "Column %d is not a valid move. Please try again";
     public static final String PRINT_MOVE = "Player %s (%s) move to %d %d";
     public static final String WINNER = "%s - %s is the winner!";
     public static final String TIE_GAME = "It's a tie game!";
