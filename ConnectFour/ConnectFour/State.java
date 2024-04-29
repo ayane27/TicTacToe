5d82cdf58c194e0256be4cd3b7e994612851c443
Ayane Sok <ayane.sok@mrjaffesclass.com>
2024-04-29 11:44:13 -0700

    comments

---
+++ ConnectFour/ConnectFour/State.java
@@ -0,0 +1,124 @@
+package tictactoe;
+
+/**
+ * Tic-Tac-Toe state variables.
+ */
+public class State
+{
+    private int gameState = Constants.STANDBY;
+    private int whoseMove = Constants.X;
+    private String xName = "";
+    private String oName = "";
+    private int[][] board = new int[Constants.BOARD_SIZE_ROW][Constants.BOARD_SIZE_COLUMN];
+
+    public boolean isWinner() {
+        int total;
+        for (int row = 0; row < Constants.BOARD_SIZE_ROW; row++) {
+            for (int col = 0; col < Constants.BOARD_SIZE_COLUMN - 3; col++) {
+                total = getBoardCell(row, col) + getBoardCell(row, col + 1) +
+                            getBoardCell(row, col + 2) + getBoardCell(row, col + 3);
+                if (total == 4 || total == -4) {
+                    return true; 
+                }
+            }
+        }
+        for (int row = 0; row < Constants.BOARD_SIZE_ROW - 3; row++) {
+            for (int col = 0; col < Constants.BOARD_SIZE_COLUMN; col++) {
+                total = getBoardCell(row, col) + getBoardCell(row + 1, col) +
+                            getBoardCell(row + 2, col) + getBoardCell(row + 3, col);
+                if (total == 4 || total == -4) {
+                    return true; 
+                }
+            }
+        }
+        for (int row = 3; row < Constants.BOARD_SIZE_ROW; row++) {
+            for (int col = 0; col < Constants.BOARD_SIZE_COLUMN - 3; col++) {
+                total = getBoardCell(row, col) + getBoardCell(row - 1, col + 1) +
+                            getBoardCell(row - 2, col + 2) + getBoardCell(row - 3, col + 3);
+                if (total == 4 || total == -4) {
+                    return true; 
+                }
+            }
+        }
+         for (int row = 0; row < Constants.BOARD_SIZE_ROW - 3; row++) {
+            for (int col = 0; col < Constants.BOARD_SIZE_COLUMN - 3; col++) {
+                total = getBoardCell(row, col) + getBoardCell(row + 1, col + 1) +
+                            getBoardCell(row + 2, col + 2) + getBoardCell(row + 3, col + 3);
+                if (total == 4 || total == -4) {
+                    return true; 
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean isTie() {
+        for (int row=0; row<Constants.BOARD_SIZE_ROW; row++) {
+            for (int col=0; col<Constants.BOARD_SIZE_COLUMN; col++) {
+                if (getBoardCell(row,col) == Constants.BLANK) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public int dropChecker(int col) {
+        col--;
+        for (int row = Constants.BOARD_SIZE_ROW-1; row >= 0; row--) {
+            if (board[row][col] == Constants.BLANK) {
+                board[row][col] = whoseMove;
+                return row;
+            }
+        }
+        return -1;
+    }
+    public int getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+
+    public int getWhoseMove() {
+        return whoseMove;
+    }
+
+    public void setWhoseMove(int whoseMove) {
+        this.whoseMove = whoseMove;
+    }
+
+    public String getXName() {
+        return xName;
+    }
+
+    public void setXName(String xName) {
+        this.xName = xName;
+    }
+
+    public String getOName() {
+        return oName;
+    }
+
+    public void setOName(String oName) {
+        this.oName = oName;
+    }
+
+    public int getBoardCell(int row, int col) {
+        return this.board[row][col];
+    }
+    public void setBoardCell(int row, int col, int value) {
+       if (row >= 0 && row < Constants.BOARD_SIZE_ROW && col >= 0 && col < Constants.BOARD_SIZE_COLUMN) {
+        this.board[row][col] = value;
+    
+    }
+    }
+    public void clearBoard() {
+        for (int row = 0; row < Constants.BOARD_SIZE_ROW; row++) {
+        for (int col = 0; col < Constants.BOARD_SIZE_COLUMN; col++) {
+            setBoardCell(row, col, Constants.BLANK);
+        }
+    }
+    }
+}
---
+++ ConnectFour/ConnectFour/UI.java
@@ -0,0 +1,110 @@
+package tictactoe;
+import java.util.Scanner;
+
+/**
+ * UI class
+ */
+public class UI
+{
+
+    Scanner scanner;
+
+    public UI() {
+        scanner = new Scanner(System.in);         
+    }
+
+    // Utility methods
+    public String getXOrO(int whoseMove) {
+        if (whoseMove == -1) {
+            return "X";
+        } else if (whoseMove == 1) {
+            return "O";
+        } else {
+            return " ";
+        }
+    }
+
+    public String getPlayerName(int whoseMove, String xName, String yName) {
+        return (whoseMove == -1) ? xName : yName;
+    }
+
+    public boolean isLegalMove(State state, int col) {
+        return  1 <= col && col<= Constants.BOARD_SIZE_COLUMN && 
+            state.getBoardCell(0, col-1) == Constants.BLANK;
+    
+    }
+
+    // Prompt for input methods
+    public String promptForName(String player) {
+        System.out.printf(Constants.GET_PLAYER_NAME, player);
+        return scanner.next();
+    }
+
+    
+    public int getMoveCol(int whoseMove, String xName, String oName) {
+        int col=0;
+        while (col <= 0 || col > Constants.BOARD_SIZE_COLUMN) {
+            try {         
+                System.out.printf(Constants.GET_COL_MOVE, getXOrO(whoseMove), getPlayerName(whoseMove, xName, oName));
+                col = scanner.nextInt();
+                if (col<= 0 || col > Constants.BOARD_SIZE_COLUMN) {
+                    System.out.println(Constants.INVALID_COLUMN)
+                }
+            } catch (Exception e) {
+                System.out.println(Constants.INVALID_COLUMN);
+                scanner.next()
+            }
+        }
+        return col;
+    }
+
+    public boolean startNewGame() {
+        System.out.println(Constants.START_NEW_GAME);
+        String yesOrNo = scanner.next();
+        return yesOrNo.equals("Y") || yesOrNo.equals("y");
+    }
+
+    // Printing text methods
+    public void printWelcome() {
+        System.out.println(Constants.TITLE);
+    }
+
+    public void printBoard(State state) {
+        System.out.println(Constants.DIVIDER_STRING);
+        for (int row = 0; row < Constants.BOARD_SIZE_ROW; row++) {
+            System.out.printf(Constants.BOARD_STRING, getXOrO(state.getBoardCell(row, 0)), getXOrO(state.getBoardCell(row, 1)), getXOrO(state.getBoardCell(row, 2)), getXOrO(state.getBoardCell(row, 3)), getXOrO(state.getBoardCell(row, 4)), getXOrO(state.getBoardCell(row, 5)), getXOrO(state.getBoardCell(row, 6)));
+            System.out.println();
+            System.out.println(Constants.DIVIDER_STRING);
+        }
+    }
+
+    public void printInvalidColumn() {
+        System.out.printf(Constants.INVALID_COLUMN);
+    }
+
+    public void printInvalidMove(int row, int col) {
+        System.out.printf(Constants.INVALID_MOVE_ERROR, row, col);
+    }
+
+    public void printMove(State state, int row, int col) {
+        System.out.printf(
+            Constants.PRINT_MOVE, getXOrO(state.getWhoseMove()), 
+            getPlayerName(state.getWhoseMove(), state.getXName(), state.getOName()), 
+            row, 
+            col
+        );
+        System.out.println();
+    } 
+
+    public void printWinner(State state) {
+        System.out.printf(
+            Constants.WINNER, 
+            getXOrO(state.getWhoseMove()), 
+            getPlayerName(state.getWhoseMove(), state.getXName(), state.getOName()));
+        System.out.println();
+    }
+
+    public void printTieGame() {
+        System.out.println(Constants.TIE_GAME);
+    }
+}
